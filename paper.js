b8e556c3917c0ac776a956282139516a52b2ea57
wafanakha <wafanakha15@gmail.com>
2023-11-04 22:19:30 +0700

    adding variable computer and randomize computer

--- paper.js
+++ paper.js
@@ -1,3 +1,15 @@
-function round(player, comp) {}
+function round(player, comp) {
+  player.toLowerCase();
+}
 
 const player = prompt("choose your fighter");
+let computer = Math.random();
+if (computer < 0.33) {
+  computer = "paper";
+} else if (computer < 0.66) {
+  computer = "rock";
+} else {
+  computer = "scissor";
+}
+console.log(computer);
+alert(player);
